5ca5c1263372250323b6aa8e46c15cc49f778af1
Pranav Pillai <pranavpillai04@gmail.com>
2023-05-20 23:46:58 -0500

    Initial Google Auth

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,24 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const User = require('../models/User');
+
+
+module.exports = function (passport) {
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID, // from Google Developer Console
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET, // from Google Developer Console
+        callbackURL: '/auth/google/callback' // from Google Developer Console
+    },
+        async (accessToken, refreshToken, profile, done) => {
+            console.log(profile);
+        }
+    ));
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => done(err, user));
+    });
+}
