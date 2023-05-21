f2c986ef7d9266e189ccdc08bb25a043224e594f
Pranav Pillai <pranavpillai04@gmail.com>
2023-05-21 16:01:56 -0500

    Added stories to dashboard

--- app.js
+++ app.js
@@ -1,56 +1,64 @@
-const path = require('path');
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const exphbs = require('express-handlebars');
-const passport = require('passport');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const connectDB = require('./config/db');
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const connectDB = require("./config/db");
 
 // Load config
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 // Passport config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Connect to database
 connectDB();
 
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({ extended: false })); // allows us to use form data in req.body
+app.use(express.json()); // allows us to accept JSON data in req.body
+
 // Logging
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Handlebars
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
-app.set('view engine', '.hbs');
+app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 // Sessions
-app.use(session({
-    secret: 'keyboard cat', // use whatever you want
+app.use(
+  session({
+    secret: "keyboard cat", // use whatever you want
     resave: false,
     saveUninitialized: false,
     // store: new MongoStore({ mongooseConnection: mongoose.connection }) // deprecated
-    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
-}));
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  })
+);
 
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
-app.use('/', require('./routes/index'));
-app.use('/auth', require('./routes/auth'));
-app.use('/stories', require('./routes/stories'));
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
