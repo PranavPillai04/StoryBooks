c1eb7bc100412a5e80e7bbeb84ca1abbbfd0b793
Pranav Pillai <pranavpillai04@gmail.com>
2023-05-22 13:04:09 -0500

    Create capitalize helper

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -47,4 +47,10 @@ module.exports = {
         ' selected="selected"$&'
       );
   },
+  capitalize: function (value) {
+    if (typeof value !== "string") {
+      return "";
+    }
+    return value.charAt(0).toUpperCase() + value.slice(1);
+  },
 };
